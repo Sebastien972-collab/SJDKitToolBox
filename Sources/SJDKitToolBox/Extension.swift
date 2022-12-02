e799446adedbf6d6dad7acf305b8d829ff2221c3
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2022-12-02 12:19:39 +0100

    FirstCommit

---
+++ Sources/SJDKitToolBox/Extension.swift
@@ -0,0 +1,57 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sébastien DAGUIN on 02/12/2022.
+//
+
+import Foundation
+
+public extension String {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+    /// Returns an array of strings separated by a character
+     func splitString(with elem : String  ) -> [String] {
+        var stringToReturn : [String] = []
+        
+        if #available(macOS 13.0, *) {
+            let stringSplited = self.split(separator: elem)
+            for sequence in stringSplited {
+                let newString = String(sequence)
+                if !newString.isEmpty {
+                    stringToReturn.append(newString)
+                }
+            }
+            return stringToReturn
+        } else {
+            // Fallback on earlier versions
+            return stringToReturn
+        }
+    }
+    /// A function that returns a word with only letters
+    var clearWord: String {
+        var newWord : [Character] = []
+        for letter in self {
+            if letter.isLetter {
+                newWord.append(letter)
+            }
+        }
+        return String(newWord)
+    }
+    /// Check if string contains a number and return a bolean
+    var containsAnumber: Bool {
+        for character in self {
+            if character.isNumber {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+public extension Array {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
---
+++ Tests/SJDKitToolBoxTests/StringExtensionTest.swift
@@ -0,0 +1,31 @@
+import XCTest
+@testable import SJDKitToolBox
+
+final class SJDKitToolBoxTests: XCTestCase {
+    func test1() {
+        let sliptedString = "Lundi, Mardi, Mecredi, Jeudi, Vendredi, Samedi, Dimanche".splitString(with: ",")
+        for elem in sliptedString {
+            print(elem)
+        }
+        XCTAssertEqual(sliptedString.count, 7)
+    }
+    func test2() {
+        let string = "Lundi, Mardi, Mecredi, Jeudi, Vendredi, Samedi, Dimanche, "
+        
+        XCTAssertTrue(string.isNotEmpty)
+    }
+    func test3() {
+        let string = "Lun7di".clearWord
+        let string2 = "Lun,di".clearWord
+        let string3 = "Lun&di".clearWord
+        let string4 = "Lun&()!§<<<>>>>/.?di".clearWord
+        XCTAssertEqual(string, "Lundi")
+        XCTAssertEqual(string2, "Lundi")
+        XCTAssertEqual(string3, "Lundi")
+        XCTAssertEqual(string4, "Lundi")
+    }
+    func tes3() {
+        XCTAssertTrue("LUNDI7".containsAnumber)
+        XCTAssertFalse("Lundi".containsAnumber)
+    }
+}
